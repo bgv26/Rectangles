087963e7a8b34568e06ae0828dc5a084c810a291
bgv26 <bgv26@mail.ru>
2016-04-03 12:24:43 +0300

    Almost final version

--- rectangles/rectangles.py
+++ rectangles/rectangles.py
@@ -1,111 +1,46 @@
-lines = '''
-  +--+
-  |  |
-+-+--+
-| |  |
-+-+--+
-'''
-
-# lines = ["+-+",
-#          "| |",
-#          "+-+",
-#          ]
-#
-# lines = ["  +-+",
-#          "  | |",
-#          "+-+-+",
-#          "| |  ",
-#          "+-+  "
-#          ]
-#
-# lines = ["  +-+",
-#          "  | |",
-#          "+-+-+",
-#          "| | |",
-#          "+-+-+"
-#          ]
-#
-# lines = ["  +-+",
-#          "    |",
-#          "+-+-+",
-#          "| | -",
-#          "+-+-+"
-#          ]
-#
-# lines = ["+------+----+",
-#          "|      |    |",
-#          "+---+--+    |",
-#          "|   |       |",
-#          "+---+-------+"
-#          ]
-#
-lines = ["+------+----+",
-         "|      |    |",
-         "+------+    |",
-         "|   |       |",
-         "+---+-------+"
-         ]
+import re
 
 
-def get_horizontal_sides(string=''):
-    rectangles = []
+def count(string=''):
     if type(string) is str:
         lines = [s for s in string.split('\n') if s]
     else:
         lines = string
+    rectangles = {}
     line_num = 0
     for line in lines:
-        tmp = line.split('+')
-        if len(tmp) > 2:
-            offset = 0
-            first = True
-            for s in tmp:
-                length = len(s)
-                if first:
-                    offset += length
-                    first = False
-                else:
-                    length += offset + 1
-                    if len(set(s)) == 1 and '-' in s:
-                        down_side = get_rectangle(lines, line_num, offset, length)
-                        if down_side:
-                            rectangles.append((line_num, offset, length, down_side))
-                    offset = length
+        hor_side = re.compile(r"(?<=\+)(-*)(?=\+)")
+        matches = [(match.start(), match.end()) for match in hor_side.finditer(line)]
+        sides = len(matches)
+        for i in range(sides):
+            right_end = matches[i][1]
+            for j in range(i + 1, sides):
+                if right_end + 1 == matches[j][0]:
+                    matches.append((matches[i][0], matches[j][1]))
+                    right_end = matches[j][1]
+        if matches:
+            rectangles[line_num] = matches
         line_num += 1
-    return rectangles
+
+    num_of_rectangles = 0
+    line_num = rectangles.keys()
+    hor_side_num = len(line_num)
+    for i in range(hor_side_num):
+        for j in range(i + 1, hor_side_num):
+            upside_num = line_num[i]
+            downside_num = line_num[j]
+            for up_side in rectangles[upside_num]:
+                if up_side in rectangles[downside_num]:
+                    num_of_rectangles += get_rectangle(lines, upside_num, downside_num, up_side)
+
+    return num_of_rectangles
 
 
-def get_rectangle(lines, line_num, start, end):
-    if line_num < len(lines) - 1:
-        line_num += 1
-    else:
-        return 0
-    while True:
-        left_side = lines[line_num][start]
-        right_side = lines[line_num][end]
-        if  left_side == '|' and right_side == '|':
-            if line_num < len(lines) - 1:
-                line_num += 1
-            else:
-                return 0
-        elif left_side == '+' and right_side == '+':
-            return line_num
-        else:
+def get_rectangle(lines, upside_num, downside_num, up_side):
+    start = up_side[0] - 1
+    end = up_side[1]
+    for i in range(upside_num + 1, downside_num):
+        if lines[i][start] not in ('|', '+') or lines[i][end] not in ('|', '+'):
             return 0
 
-print get_horizontal_sides(lines)
-
-def get_side(string = ''):
-    if type(string) is str:
-        lines = [s for s in string.split('\n') if s]
-    else:
-        lines = string
-    for line in lines:
-        left = line.find('+')
-        if left >= 0:
-            left += 1
-            while line[left] == '-':
-                left += 1
-#                 регулярочка "\+(\-+)(?=\+)"
-
-print get_side(lines)
+    return 1
